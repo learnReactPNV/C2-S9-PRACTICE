2355618656816e14a75dddaf7152c6f0c5d37086
Huyen Ho <hothihuyen06031195@gmail.com>
2024-06-08 10:51:13 +0700

    DONE EXERCISE 3

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
   /* You will need to use many state to keep the inut values and other needs */
+  const [isError, setIsError] = useState(false);
+  const [keyA, setkeyA] = useState('');
+  const [keyB, setkeyB] = useState('');
+  const [outPut, setOutPut] = useState('');
 
   /* You will need some function to handle the key pressed and button events */
+  const onA = (event) => {
+    setkeyA(event.target.value);
+  }
+
+  const onB = (event) => {
+    setkeyB(event.target.value);
+  }
+
+  const handleCompute = () => {
+    if (isNaN(keyA) || isNaN(keyB) || keyA === '' || keyB === '') {
+      setIsError(true);
+      setOutPut("A and B shall be numbers!");
+    } else {
+      setIsError(false);
+      setOutPut(parseInt(keyA) + parseInt(keyB));
+    }
+  }
 
   return (
     <main>
@@ -18,8 +40,8 @@ function App() {
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input className={isError? "error" : ''} value={outPut} disabled />
+      <button onClick={handleCompute} >Compute</button>
     </main>
   );
 }
